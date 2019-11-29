3d61e43a24bae05dd2a784d81065c74baf7d0abd
Tung <tctung18@apcs.vn>
2019-11-29 10:38:20 +0700

    draw car cua vu

--- gameBangQuaDuong/gameBangQuaDuong/CCAR.cpp
+++ gameBangQuaDuong/gameBangQuaDuong/CCAR.cpp
@@ -1,1 +1,43 @@
 #include "CCAR.h"
+
+void CCAR::draw()
+{
+	gotoXY(x, y);
+	cout << char(254);
+	gotoXY(x+1, y);
+	cout << char(178) << endl;
+	gotoXY(x + 2, y);
+	cout << char(254) << endl;
+}
+CCAR::CCAR() {
+	x= 0;
+	y= 25;
+}
+
+CCAR::CCAR(int a, int b) {
+	x = a;
+	y = b;
+}
+void CCAR::Move()
+{
+	gotoXY(x, y);
+	cout << " ";
+	gotoXY(x + 1, y);
+	cout << " " ;
+	gotoXY(x + 2, y);
+	cout << " ";
+	x++;
+	draw();
+	if (x+2 == 115)
+	{
+		gotoXY(x, y);
+		cout << " ";
+		gotoXY(x + 1, y);
+		cout << " ";
+		gotoXY(x + 2, y);
+		cout << " ";
+		x = 0;
+	}
+	
+	
+}
--- gameBangQuaDuong/gameBangQuaDuong/CCAR.h
+++ gameBangQuaDuong/gameBangQuaDuong/CCAR.h
@@ -1,7 +1,14 @@
 #pragma once
+#include "Header.h"
 #include "CVEHICLE.h"
 class CCAR :
 	public CVEHICLE
 {
+	int x, y;
+public:
+	CCAR();
+	CCAR(int, int);
+	void draw();
+	void Move();
 };
 
